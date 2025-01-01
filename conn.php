01757d522547185e15a8a59e42552303697e3fc1
calvintakasi <calvintakasi@outlook.com>
2025-01-01 23:41:56 +0530

    rebuilt

--- conn.php
+++ conn.php
@@ -1,37 +1,30 @@
 <?php
-$id=$_POST['id'];
-$name=$_POST['name'];
-$purpose=$_POST['purpose'];
-$mob=$_POST['mob'];
-$date=$_POST['date'];
-$intime=$_POST['intime'];
-$outtime=$_POST['outtime'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $purpose = $_POST['purpose'];
+    $mob = $_POST['mob'];
+    $date = $_POST['date'];
+    $intime = $_POST['intime'];
+    $outtime = $_POST['outtime'];
 
-//for database connection
+    // Database connection
+    $conn = new mysqli('localhost', 'root', '', 'studententry');
+    if ($conn->connect_error) {
+        die('Connection Failed: ' . $conn->connect_error);
+    } else {
+        // Prepared statement
+        $db_stmt = $conn->prepare("INSERT INTO studententry(id, name, purpose, mob, date, intime, outtime) VALUES (?, ?, ?, ?, ?, ?, ?)");
+        $db_stmt->bind_param("issssss", $id, $name, $purpose, $mob, $date, $intime, $outtime);
 
-$conn = new mysqli('localhost' , 'root' , '' , 'studententry');
-if($conn->connect_error){
-    die('Connection Failed: ' .$conn->connect_error);
-}
-else{
-    $db_stmt = $conn->prepare("insert into studententry(id , name , purpose , mob , date , intime , outtime)
-    values(?, ?, ?, ?, ? ,? ,?)");
-    $db_stmt->bind_param("issssss", $id , $name , $purpose , $mob , $date , $intime , $outtime);
+        // Execute statement
+        $db_stmt->execute();
 
-// i - integer
-// s - string
+        // Close statement and connection
+        $db_stmt->close();
+        $conn->close();
 
-// Execute the prepared databse statement
-$db_stmt->execute();
-
-// Close the prepared database statement
-$db_stmt->close();
-
-//close the connection
-$conn->close();
-
-echo "Entry Successful";
-
+        echo "Entry Successful";
+    }
 }
 ?>
-      
